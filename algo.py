8ea56c798474523dc177cce6b72ce0b4ebf1b479
arrz.dev <55147716+arrzdev@users.noreply.github.com>
2022-12-14 18:53:06 +0000

    Refactorated some of the code

--- algo.py
+++ algo.py
@@ -1,41 +1,46 @@
-#memoization table
-memo = {}
+#init memoization table
+memo_table = {}
 
-def solution(grid):
-  #max column
-  target_line_size = max(grid)
-  if target_line_size <= 1:
+def calc_tilling_ways(grid):
+  #get the biggest line size
+  max_line_size = max(grid)
+  if max_line_size <= 1:
     return 1
 
   #cache hits
   grid_hash = hash_grid(grid)
-  memo_result = memo.get(grid_hash)
-  if memo_result: return memo_result
+  memoization_result = memo_table.get(grid_hash)
+  if memoization_result: return memoization_result
   
-  target_index = grid.index(target_line_size)
+  #find the target line index
+  target_index = grid.index(max_line_size)
 
-  #find the max square that can be removed
-  segment_size = 1
+  #find the max square that can be removed at target position
+  max_square_size = 1
   for line_size in grid[target_index+1:]:
-    #max segment_size reached
-    if line_size < target_line_size or segment_size == line_size:
+    #max square size reached condition
+    if line_size < max_line_size or max_square_size == line_size:
       break
-    segment_size += 1
+    max_square_size += 1
 
-  #remove it
+  #remove every possible square and calculate the ways of tilling
   ways_of_tilling = 0
-  for square_size in range(1, segment_size+1):
+  for square_size in range(1, max_square_size+1):
     new_grid = remove_square(grid, square_size, target_index)
-    ways_of_tilling += solution(new_grid)
+    ways_of_tilling += calc_tilling_ways(new_grid)
 
-  memo[grid_hash] = ways_of_tilling
+  memo_table[grid_hash] = ways_of_tilling
   return ways_of_tilling
   
 def remove_square(grid, square_size, target_index):
-  new_grid = [*grid]
-  for i in range(target_index, target_index+square_size):
-    new_value = new_grid[i] - square_size
-    new_grid[i] = new_value if new_value > 1 else 0
+  new_grid = []
+  for i in range(len(grid)):
+    value = grid[i]
+    if i >= target_index and i < target_index+square_size:
+      new_value = value - square_size
+      value = new_value if new_value > 1 else 0
+    new_grid.append(value)
+
   return new_grid
 
 def hash_grid(grid):
@@ -54,12 +59,12 @@ def main():
   grid = []
   for _ in range(grid_height):
     line_size = int(input())
-    if line_size:
+    if line_size and not has_content:
       has_content = 1
     if line_size > 1:
       grid.append(line_size)
 
-  print(solution(grid) if len(grid) else has_content)
+  print(calc_tilling_ways(grid) if len(grid) else has_content)
 
 if __name__ == "__main__":
   main() 
